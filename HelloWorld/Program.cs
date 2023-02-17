c1cf985570787592531619c36cc4b58b5485b9dd
primolord <88434607+primolord@users.noreply.github.com>
2023-02-17 19:31:18 +0300

    fixed
    fixed

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Threading;
 
+
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+
             Console.Title = "beta loader. Writed by primolord";
 
             Console.WriteLine("Hello! Welcome in Beta Loader of *** Client!");
@@ -24,45 +27,52 @@ namespace HelloWorld
                 Console.WriteLine("Welcome, Admin");
                 Console.ReadKey();
                 Console.Clear();
-            } else
+                Console.WriteLine("You're logged in: " + user);
+                Console.WriteLine("Days: You're using lifetime version!");
+
+                Console.WriteLine("Inject?");
+                Console.WriteLine("rust (type: rust)");
+                Console.WriteLine("csgo (type: csgo)");
+                Console.WriteLine("Select:");
+                string selected = Console.ReadLine();
+
+                if (selected == "rust")
+                {
+                    Console.WriteLine("Waiting for RUST...");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Injecting...");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Injected!");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+                if (selected == "csgo")
+                {
+                    Console.WriteLine("Waiting for csgo...");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Injecting...");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Injected!");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("Press any key for close loader...");
+                Console.ReadKey();
+
+            }
+            else
             {
-                Console.WriteLine("Email or password have error!");
-                Console.WriteLine("Press any key for close loader");
                 Console.ReadKey();
             }
-            Console.WriteLine("You're logged in: " + user);
-            Console.WriteLine("Days: You're using lifetime version!");
-
-            Console.WriteLine("Inject?");
-            Console.WriteLine("rust (type: rust)");
-            Console.WriteLine("csgo (type: csgo)");
-            Console.WriteLine("Select:");
-            string selected = Console.ReadLine();
-
-            if (selected == "rust")
+            if (email != EmailCorrect || password != PasswordCorrect)
             {
-                Console.WriteLine("Waiting for RUST...");
-                Thread.Sleep(5000);
-                Console.WriteLine("Injecting...");
-                Thread.Sleep(10000);
-                Console.WriteLine("Injected!");
-            } else
-            {
-                Console.WriteLine("");
+                Console.WriteLine("Email or password have error!");
+                Console.ReadKey();
             }
-            if (selected == "csgo")
-            {
-                Console.WriteLine("Waiting for csgo...");
-                Thread.Sleep(5000);
-                Console.WriteLine("Injecting...");
-                Thread.Sleep(10000);
-                Console.WriteLine("Injected!");
-            } else
-            {
-                Console.WriteLine("");
-            }
-            Console.WriteLine("Press any key for close loader...");
-            Console.ReadKey();
         }
     }
 }
